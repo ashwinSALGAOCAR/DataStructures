8dd70fea588eca6839e35be41df88616917d9d87
ashwinSALGAOCAR <451ashwin@gmail.com>
2019-11-02 16:17:46 +0530

    comments for insert and remove method

--- binary_heap.py
+++ binary_heap.py
@@ -7,6 +7,15 @@ class MaxHeap:
 		self.heap = []
 
 	def insert(self, data):
+
+		'''We begin inserting in the heap by appending new data at the end of the list.
+		Then we get the index of the newly inserted child and the height of the heap.
+		Since the child node is at the end of the list we would be traversing up the height,
+		therefore we will loop through the heap till we reach height 0.
+		Since this is a Max Heap we compare the child node with the parent and replace each
+		other if the parent is smaller than the child.
+		'''
+
 		self.heap.append(data)
 		length = len(self.heap)
 		child_index = length
@@ -21,6 +30,16 @@ class MaxHeap:
 			height = height - 1
 
 	def delete(self):
+
+		'''We begin from the top node. The node with the heighest value and traverse down
+		the height. We loop till we reach one less than the height. In the second loop we
+		untill both child nodes exist. In this loop we move the top node down untill it is
+		pushed to the end of the list and eliminated.
+
+		If the node has only left child(even list length means it has only left child), we
+		interchange the positions of the child and eliminate the parent.
+		'''
+
 		length = len(self.heap)
 		height = math.log(length)
 		i = h = 0
@@ -38,7 +57,7 @@ class MaxHeap:
 
 			h = h + 1
 
-		if length % 2 == 0:
+		if length % 2 == 0 and 2 * i + 1 < length:
 			self.heap[i] = self.heap[2 * i + 1]
 			i = 2 * i + 1
 
@@ -52,8 +71,8 @@ class MaxHeap:
 new_heap = MaxHeap()
 new_heap.insert(25)
 new_heap.insert(2)
-new_heap.insert
 new_heap.insert(13)
+new_heap.insert(34)
 new_heap.insert(47)
 new_heap.insert(5)
 new_heap.insert(88)
@@ -69,5 +88,5 @@ new_heap.delete()
 new_heap.delete()
 new_heap.delete()
 new_heap.delete()
-new_heap.delete()
+
 new_heap.print_heap()
