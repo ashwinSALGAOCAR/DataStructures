e21eb255664156d8b8d8953e749d34ff33aa4c06
ashwinSALGAOCAR <451ashwin@gmail.com>
2019-11-02 15:41:03 +0530

    delete method for binary heap

--- binary_heap.py
+++ binary_heap.py
@@ -20,19 +20,54 @@ class MaxHeap:
 				child_index = parent_index
 			height = height - 1
 
+	def delete(self):
+		length = len(self.heap)
+		height = math.log(length)
+		i = h = 0
+		while h < height:
+			while 2 * i + 2 < length:
+				temp = self.heap[i]
+				if self.heap[2 * i + 1] > self.heap[2 * i + 2]:
+					self.heap[i] = self.heap[2 * i + 1]
+					i = 2 * i + 1
+				else:
+					self.heap[i] = self.heap[2 * i + 2]
+					i = 2 * i + 2
+
+				self.heap[i] = temp
+
+			h = h + 1
+
+		if length % 2 == 0:
+			self.heap[i] = self.heap[2 * i + 1]
+			i = 2 * i + 1
+
+		self.heap = self.heap[: -1]
+		
 	def print_heap(self):
 		print(self.heap)
+		print("Length of Heap: " + str(len(self.heap)) + "\n")
 
 
 new_heap = MaxHeap()
 new_heap.insert(25)
 new_heap.insert(2)
+new_heap.insert
 new_heap.insert(13)
 new_heap.insert(47)
 new_heap.insert(5)
+new_heap.insert(88)
 new_heap.insert(50)
 new_heap.insert(100)
 new_heap.insert(200)
 new_heap.insert(150)
 
+new_heap.print_heap()
+
+new_heap.delete()
+new_heap.delete()
+new_heap.delete()
+new_heap.delete()
+new_heap.delete()
+new_heap.delete()
 new_heap.print_heap()
